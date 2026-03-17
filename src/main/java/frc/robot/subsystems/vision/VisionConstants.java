efe31dd376bfac4b8ee0d27a55da660f5187f747
emilroy75 <emilroy819@gmail.com>
2026-03-16 21:28:47 -0400

    add everything

---
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -0,0 +1,69 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
+
+public class VisionConstants {
+  // AprilTag layout
+  public static AprilTagFieldLayout aprilTagLayout =
+      AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+  // Camera names, must match names configured on coprocessor
+  public static String camera0Name = "Front_Camera";
+  public static String camera1Name = "Left_Camera";
+  public static String Camera2Name = "Right_Camera";
+
+  // Robot to camera transforms
+  // (Not used by Limelight, configure in web UI instead)
+  public static Transform3d robotToCamera0 =
+      new Transform3d(
+          Units.inchesToMeters(2.0),
+          Units.inchesToMeters(1.0),
+          Units.inchesToMeters(10.0),
+          new Rotation3d(0.0, -0.4, 0.0));
+  public static Transform3d robotToCamera1 =
+      new Transform3d(
+          Units.inchesToMeters(2.0),
+          Units.inchesToMeters(1.0),
+          Units.inchesToMeters(10.0),
+          new Rotation3d(0.0, -0.4, Math.PI / 2));
+  public static Transform3d robotToCamera2 =
+      new Transform3d(
+          Units.inchesToMeters(2.0),
+          Units.inchesToMeters(1.0),
+          Units.inchesToMeters(10.0),
+          new Rotation3d(0.0, -0.4, -Math.PI / 2));
+
+  // Basic filtering thresholds
+  public static double maxAmbiguity = 0.3;
+  public static double maxZError = 0.75;
+
+  // Standard deviation baselines, for 1 meter distance and 1 tag
+  // (Adjusted automatically based on distance and # of tags)
+  public static double linearStdDevBaseline = 0.02; // Meters
+  public static double angularStdDevBaseline = 0.06; // Radians
+
+  // Standard deviation multipliers for each camera
+  // (Adjust to trust some cameras more than others)
+  public static double[] cameraStdDevFactors =
+      new double[] {
+        1.0, // Camera 0
+        1.0, // Camera 1
+        1.0 // Camera 2
+      };
+
+  // Multipliers to apply for MegaTag 2 observations
+  public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+  public static double angularStdDevMegatag2Factor =
+      Double.POSITIVE_INFINITY; // No rotation data available
+}
